bf9d3bea433704c243e486281e27546066842d5a
rachtachar <rachtachar.ma.66@ubu.ac.th>
2026-01-30 15:42:09 +0700

    almost doneฒ

---
+++ main.py
@@ -0,0 +1,24 @@
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(f"(self.name) is (self.age) years old.")
+
+    def __str__(self):
+        return f"{self.name} is {self.age} years."
+
+    def main():
+        my_dog = Dog("Buddy", 3)
+        your_dog = Dog("Peter, 2")
+        print(my_dog) 
+
+
+
+def main():
+    print("Hello from week09!")
+
+
+if __name__ == "__main__":
+    main()
